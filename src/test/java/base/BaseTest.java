865426b0aee991b3673ce05ff2a5cd7091e3a4e5
Szalma <stefania.szalma@gmail.com>
2021-08-17 14:35:49 +0200

    done

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -25,7 +25,7 @@ public class BaseTest {
         ChromeOptions options=new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-       // options.addArguments("--headless");
+        options.addArguments("--headless");
         driver=new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
